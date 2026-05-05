2439c2cf134c89a5293e4d99925bd82d502ea540
SamuelMesa01 <est.samue.mesa@unimilitar.edu.co>
2026-05-05 08:49:27 -0500

    Leer arreglo de jefes

--- src/Subordinates.java
+++ src/Subordinates.java
@@ -4,5 +4,10 @@ public class Subordinates {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        
+        int[] boss = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            boss[i] = sc.nextInt();
+        }
     }
 }
