f7a6077631d4565462e1bc58aedb73a28bc7748d
SamuelMesa01 <est.samue.mesa@unimilitar.edu.co>
2026-05-05 08:53:44 -0500

    Construir lista de adyacencia

--- src/Subordinates.java
+++ src/Subordinates.java
@@ -9,5 +9,13 @@ public class Subordinates {
         for (int i = 2; i <= n; i++) {
             boss[i] = sc.nextInt();
         }
+        List<Integer>[] tree = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 2; i <= n; i++) {
+            tree[boss[i]].add(i);
+        }
     }
 }
